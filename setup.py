670c0d78c5d3d6bd38008c32b15cb8dff0b3fda1
Bartek Rutkowski <contact@robakdesign.com>
2015-07-13 16:04:29 +0100

    * Added initial setup.py and empty __init__.py files

---
+++ setup.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+from os.path import join
+from setuptools import setup, find_packages
+
+setup(name='ldapass',
+            version='0.1',
+            description='Web application for setting/changing LDAP user passwords.'
+            author='Bartek Rutkowski',
+            author_email='contact+ldapass@robakdesign.com',
+            license='BSD3',
+            url='https://github.com/bartekrutkowski/ldapass',
+            packages=find_packages(),
+      entry_points = {
+                     'console_scripts': ['ldapass = ldapass:main',]
+      },
+      install_requires=['Flask', 'WTForms', 'python-ldap'],
+      )
