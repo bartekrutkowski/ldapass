5a5e302e65f6dafb28d5cd61148662f3dee20312
Bartek Rutkowski <contact@robakdesign.com>
2015-07-14 13:34:10 +0100

    * Added MANIFEST.in for installing flask templates and static files * Added include_package_data=True to setup.py for installing flask templates and static files * Removed -e from pip install in travis.yml (shouldnt be needed anymore, since the app should install fine from now on)

--- setup.py
+++ setup.py
@@ -2,14 +2,16 @@
 
 from setuptools import setup, find_packages
 
-setup(name='ldapass',
-      version='0.1',
-      description='Web application for setting/changing LDAP user passwords.',
-      author='Bartek Rutkowski',
-      author_email='contact+ldapass@robakdesign.com',
-      license='BSD3',
-      url='https://github.com/bartekrutkowski/ldapass',
-      packages=find_packages(),
-      entry_points = {'console_scripts': ['ldapass=ldapass:main']},
-      install_requires=['Flask', 'WTForms', 'python-ldap'],
-      )
+setup(
+    name='ldapass',
+    version='0.1',
+    description='Web application for setting/changing LDAP user passwords.',
+    author='Bartek Rutkowski',
+    author_email='contact+ldapass@robakdesign.com',
+    license='BSD3',
+    url='https://github.com/bartekrutkowski/ldapass',
+    packages=find_packages(),
+    include_package_data=True,
+    entry_points = {'console_scripts': ['ldapass=ldapass:main']},
+    install_requires=['Flask', 'WTForms', 'python-ldap']
+)
